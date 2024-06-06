c191c17c8159abab07e3141989c9a2283c58b836
Git-Yuya <f.l.tmufbbfim60@gmail.com>
2024-06-07 01:58:19 +0900

    Add comments to utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,15 @@ import random
 import numpy as np
 import torch
 
+
 def set_seed(seed: int = 0) -> None:
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
+    """Set the random seed for reproducibility.
+    
+    Args:
+        seed (int): Random seed value.
+    Returns:
+        None
+    """
+    random.seed(seed)  # Setting random seed for Python's random module
+    np.random.seed(seed)  # Setting random seed for numpy
+    torch.manual_seed(seed)  # Setting random seed for PyTorch
